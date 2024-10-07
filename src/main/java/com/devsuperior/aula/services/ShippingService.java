ef64791458e7dfe052beb98c278de1089f45e12a
Douglas Engratulis <dengratulis@gmail.com>
2024-10-07 01:09:24 -0300

    Shipping method

--- src/main/java/com/devsuperior/aula/services/ShippingService.java
+++ src/main/java/com/devsuperior/aula/services/ShippingService.java
@@ -1,4 +1,15 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.entities.Order;
+
 public class ShippingService {
+
+    public double shipment(Order order) {
+        if (order.getBasic() < 100.00) {
+            return 20.00;
+        } else if (order.getBasic() < 200.00) {
+            return 12.00;
+        }
+        return 0;
+    }
 }
