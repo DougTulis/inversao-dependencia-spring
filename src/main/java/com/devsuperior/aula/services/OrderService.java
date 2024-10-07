f3ed18fa5608536407ab6ac42a05a490f8dbbeb0
Douglas Engratulis <dengratulis@gmail.com>
2024-10-07 01:26:39 -0300

    Total Method

--- src/main/java/com/devsuperior/aula/services/OrderService.java
+++ src/main/java/com/devsuperior/aula/services/OrderService.java
@@ -1,4 +1,15 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Order order) {
+        return order.getBasic() - order.getDiscount() -  shippingService.shipment(order);
+    }
 }
